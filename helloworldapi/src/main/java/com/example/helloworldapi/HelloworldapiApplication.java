9bca3599781872bbd4f3a01b1b7b13378acfb138
Sirisha Satish <sirishasatish1998@gmail.com>
2025-01-17 11:36:53 -0600

    Add Hello World API

--- helloworldapi/src/main/java/com/example/helloworldapi/HelloworldapiApplication.java
+++ helloworldapi/src/main/java/com/example/helloworldapi/HelloworldapiApplication.java
@@ -2,12 +2,23 @@ package com.example.helloworldapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class HelloworldapiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloworldapiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloworldapiApplication.class, args);
+    }
 
+    @RestController
+    class HelloWorldController {
+
+        @GetMapping("/hello")
+        public String helloWorld() {
+            return "Hello World";
+        }
+    }
 }
+
